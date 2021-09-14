de10b104e169d441d274758ea5f53b180aaf634e
Longarithm <the.aleksandr.logunov@gmail.com>
2021-09-15 01:25:20 +0300

    test

--- runtime/runtime-params-estimator/src/vm_estimator.rs
+++ runtime/runtime-params-estimator/src/vm_estimator.rs
@@ -217,8 +217,11 @@ pub(crate) fn least_squares_method_2(
     let a = x_train.clone().insert_column(cols - 1, 1.0).into_owned();
     let b = y_train.clone().transpose();
     let x = (a.transpose() * &a).try_inverse().unwrap() * &a.transpose() * &b;
+    println!("aaaaaaaaa");
     let coeff: Vec<f64> = x.columns(0, cols - 1).iter().cloned().collect();
+    println!("aaaaaaaaa");
     println!("{:?}", coeff.len());
+    println!("aaaaaaaaa");
     let intercept = x[(cols - 1, 0)];
     (coeff, intercept)
 }
