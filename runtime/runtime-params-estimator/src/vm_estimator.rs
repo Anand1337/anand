9992043a796db0ac0da90a3ed91edb609e34bb06
Longarithm <the.aleksandr.logunov@gmail.com>
2021-09-15 01:13:04 +0300

    test

--- runtime/runtime-params-estimator/src/vm_estimator.rs
+++ runtime/runtime-params-estimator/src/vm_estimator.rs
@@ -211,7 +211,7 @@ pub(crate) fn least_squares_method_2(
     xs: &DMatrix<f64>,
     ys: &DVector<f64>,
     cols: usize,
-) -> (DMatrix<f64>, f64) {
+) -> (Matrix<f64>, f64) {
     let x_train = xs;
     let y_train = ys.transpose();
     let a = x_train.clone().insert_column(cols - 1, 1.0).into_owned();
