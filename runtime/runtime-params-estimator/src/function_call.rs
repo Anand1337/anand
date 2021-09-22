ef68979f88e199de6ab3c2d5a68d3213c108afec
Longarithm <the.aleksandr.logunov@gmail.com>
2021-09-23 02:05:17 +0300

    test

--- runtime/runtime-params-estimator/src/function_call.rs
+++ runtime/runtime-params-estimator/src/function_call.rs
@@ -392,7 +392,7 @@ fn compare_function_call_icount() {
 
         let cost = compute_function_call_cost(
             GasMetric::ICount,
-            VMKind::Wasmer0,
+            VMKind::Wasmer1,
             REPEATS,
             &contract,
             method_name,
