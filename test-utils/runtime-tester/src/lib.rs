558b8e21142d14bcf9fd6fca16c03cabed6234b6
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-10-04 12:45:56 +0300

    repro for #4771
        cargo test --release --package runtime-tester --lib \
            --features nearcore/no_cache \
            -- perf_cliff --exact --nocapture

--- test-utils/runtime-tester/src/lib.rs
+++ test-utils/runtime-tester/src/lib.rs
@@ -51,3 +51,48 @@ fn scenario_smoke_test() {
 
     scenario.run().result.unwrap();
 }
+
+#[test]
+fn perf_cliff() {
+    use near_crypto::{InMemorySigner, KeyType};
+    use near_primitives::transaction::Action;
+    use near_primitives::transaction::DeployContractAction;
+    use near_primitives::types::AccountId;
+
+    let code = vec![92; 256];
+    let num_accounts = 200_000;
+    let block_size = 100;
+    let n_blocks = 120;
+
+    let seeds: Vec<String> = (0..num_accounts).map(|i| format!("near_{}_{}", i, i)).collect();
+    let accounts: Vec<AccountId> = seeds.iter().map(|id| id.parse().unwrap()).collect();
+
+    let mut s = Scenario {
+        network_config: NetworkConfig { seeds: seeds },
+        blocks: Vec::new(),
+        use_in_memory_store: false,
+    };
+
+    let mut i = 0;
+    for h in 0..n_blocks {
+        let mut block = BlockConfig::at_height((h + 1) as u64);
+        for _ in 0..block_size {
+            let signer_id = accounts[i].clone();
+            let receiver_id = signer_id.clone();
+            let signer =
+                InMemorySigner::from_seed(signer_id.clone(), KeyType::ED25519, signer_id.as_ref());
+            block.transactions.push(TransactionConfig {
+                nonce: i as u64,
+                signer_id,
+                receiver_id,
+                signer,
+                actions: vec![Action::DeployContract(DeployContractAction { code: code.clone() })],
+            });
+            i = (i + 97) % accounts.len();
+        }
+        s.blocks.push(block)
+    }
+
+    tracing_span_tree::span_tree().aggregate(true).enable();
+    s.run().result.unwrap();
+}
