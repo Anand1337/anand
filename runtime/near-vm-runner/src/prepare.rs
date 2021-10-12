9305af45ecf6537ddf866f395580779f373ce34f
Longarithm <the.aleksandr.logunov@gmail.com>
2021-10-12 20:35:52 +0300

    test

--- runtime/near-vm-runner/src/prepare.rs
+++ runtime/near-vm-runner/src/prepare.rs
@@ -195,8 +195,8 @@ pub fn prepare_contract(
         .into_wasm_code()
 }
 
-pub fn get_functions_number(original_code: &[u8], config: &VMConfig) -> Result<u64, PrepareError> {
-    ContractModule::init(original_code, config)?.get_functions_number()?
+pub fn get_functions_number(original_code: &[u8], config: &VMConfig) -> u64 {
+    ContractModule::init(original_code, config).unwrap().get_functions_number()
 }
 
 #[cfg(test)]
