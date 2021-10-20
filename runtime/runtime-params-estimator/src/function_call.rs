68afcca4a599b2a69d7ea77a53eb3ed286170b3e
Longarithm <the.aleksandr.logunov@gmail.com>
2021-10-20 20:32:19 +0300

    update wasmparser

--- runtime/runtime-params-estimator/src/function_call.rs
+++ runtime/runtime-params-estimator/src/function_call.rs
@@ -3,20 +3,25 @@ use crate::testbed_runners::{end_count, start_count, GasMetric};
 use crate::vm_estimator::{create_context, least_squares_method};
 use near_primitives::config::VMConfig;
 use near_primitives::contract::ContractCode;
+use near_primitives::runtime::config_store::RuntimeConfigStore;
 use near_primitives::runtime::fees::RuntimeFeesConfig;
 use near_primitives::types::{CompiledContractCache, ProtocolVersion};
 use near_store::{create_store, StoreCompiledContractCache};
+use near_test_contracts::many_functions_contract;
 use near_vm_logic::mocks::mock_external::MockedExternal;
+use near_vm_runner::prepare::prepare_contract;
 use near_vm_runner::{run_vm, VMKind};
 use nearcore::get_store_path;
+use num_rational::Ratio;
 use std::fmt::Write;
 use std::sync::Arc;
+
+const REPEATS: u64 = 15;
 
 #[allow(dead_code)]
 fn test_function_call(metric: GasMetric, vm_kind: VMKind) {
     let mut xs = vec![];
     let mut ys = vec![];
-    const REPEATS: u64 = 50;
     for method_count in vec![5, 20, 30, 50, 100, 200, 1000] {
         let contract = make_many_methods_contract(method_count);
         let cost = compute_function_call_cost(metric, vm_kind, REPEATS, &contract);
@@ -39,6 +44,51 @@ fn test_function_call(metric: GasMetric, vm_kind: VMKind) {
         ratio_to_gas_signed(metric, cost_base),
         ratio_to_gas_signed(metric, cost_byte),
     );
+}
+
+#[allow(dead_code)]
+fn test_prepare_contract(metric: GasMetric) {
+    for (method_count, _) in
+        // vec![(2, 1), (5, 1), (10, 1), (100, 1), (1000, 1), (10000, 1)].iter().cloned()
+        vec![(10010, 1), (20010, 1), (50010, 1), (100010, 1)].iter().cloned()
+    // vec![(0, 0)].iter().cloned()
+    {
+        let code = many_functions_contract(method_count);
+        // let contract = ContractCode::new(code, None);
+        let start = start_count(metric);
+        let store = RuntimeConfigStore::new(None);
+        let config = store.get_config(ProtocolVersion::MAX);
+        let vm_config = &config.wasm_config;
+        for i in 0..REPEATS {
+            print!("{} ", i);
+            let result = prepare_contract(&code, vm_config);
+            if method_count < 10000 {
+                assert!(result.is_ok());
+            } else {
+                assert!(result.is_err());
+            }
+        }
+        let total_raw = end_count(metric, &start) as i128;
+
+        println!(
+            "total cost = {}, average gas cost = {}, len = {}",
+            total_raw,
+            ratio_to_gas_signed(metric, Ratio::new(total_raw as i128, REPEATS as i128)),
+            code.len()
+        );
+    }
+}
+
+#[test]
+fn test_prepare_contract_icount() {
+    // Use smth like
+    // CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER=./runner.sh \
+    // cargo test --release --features no_cpu_compatibility_checks,required  \
+    // --lib function_call::test_prepare_contract_icount -- --exact --nocapture
+    // Where runner.sh is
+    // /host/nearcore/runtime/runtime-params-estimator/emu-cost/counter_plugin/qemu-x86_64 \
+    // -cpu Westmere-v1 -plugin file=/host/nearcore/runtime/runtime-params-estimator/emu-cost/counter_plugin/libcounter.so $@
+    test_prepare_contract(GasMetric::ICount);
 }
 
 #[test]
