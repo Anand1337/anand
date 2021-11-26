e2d9e03ac86d3bec720dec67a1cbacdd49707334
Longarithm <the.aleksandr.logunov@gmail.com>
2021-11-26 20:55:18 +0300

    comments

--- runtime/runtime-params-estimator/src/function_call.rs
+++ runtime/runtime-params-estimator/src/function_call.rs
@@ -161,29 +161,36 @@ fn test_function_call_all_codes(metric: GasMetric, vm_kind: VMKind) {
     let mut estimated_codes: Vec<EstimatedCode> = Vec::new();
 
     // prepare mainnet contracts
-    let contracts_bytes =
-        std::fs::read("/host/nearcore/codes.json").expect("Could not open codes file.");
-    let entries: Vec<(Vec<u8>, String)> = serde_json::from_slice(&contracts_bytes).unwrap();
-    for (code, account_id) in entries.iter() {
-        if code.is_empty() || account_id != "cdao.near" {
-            continue;
-        }
-        let code = blow_up_code(code, 100);
-        estimated_codes.push(EstimatedCode {
-            id: format!("from_mainnet_with_noop_increased_{}.{}", 100, account_id),
-            code,
-        });
-    }
+    // let contracts_bytes =
+    //     std::fs::read("/host/nearcore/codes.json").expect("Could not open codes file.");
+    // let entries: Vec<(Vec<u8>, String)> = serde_json::from_slice(&contracts_bytes).unwrap();
+    // for (code, account_id) in entries.iter() {
+    //     if code.is_empty() || account_id != "cdao.near" {
+    //         continue;
+    //     }
+    //     let code = blow_up_code(code, 100);
+    //     estimated_codes.push(EstimatedCode {
+    //         id: format!("from_mainnet_with_noop_increased_{}.{}", 100, account_id),
+    //         code,
+    //     });
+    // }
 
     // prepare params
-    for (method_count, body_repeat) in
-        vec![(2, 1), (5, 1), (10, 1), (100, 1), (1000, 1), (9990, 1), (9990, 10), (9990, 110)]
-            .iter()
-            .cloned()
-    {
-        let code = many_functions_contract_with_repeats(method_count, body_repeat);
-        estimated_codes
-            .push(EstimatedCode { id: format!("many_fns_{}_{}", method_count, body_repeat), code });
+    // for (method_count, body_repeat) in
+    //     vec![(2, 1), (5, 1), (10, 1), (100, 1), (1000, 1), (9990, 1), (9990, 10), (9990, 110)]
+    //         .iter()
+    //         .cloned()
+    // {
+    //     let code = many_functions_contract_with_repeats(method_count, body_repeat);
+    //     estimated_codes
+    //         .push(EstimatedCode { id: format!("many_fns_{}_{}", method_count, body_repeat), code });
+    // }
+
+    // parse files
+    for fname in vec!["cdao_test.near.wasm"].iter().cloned() {
+        let fpath = format!("/host/nearcore/contracts/{}", fname);
+        let code = std::fs::read(fpath).expect("Could not open codes file.");
+        estimated_codes.push(EstimatedCode { id: format!("{}", fname), code });
     }
 
     // estimate
