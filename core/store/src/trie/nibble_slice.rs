f493bfbb31b76ebdc2879d8221dae72e91474e33
Longarithm <the.aleksandr.logunov@gmail.com>
2022-01-31 15:03:41 +0300

    debug

--- core/store/src/trie/nibble_slice.rs
+++ core/store/src/trie/nibble_slice.rs
@@ -73,6 +73,13 @@ impl<'a> NibbleSlice<'a> {
         NibbleSlice::new_offset(data, 0)
     }
 
+    pub(crate) fn until_offset(&self, full: bool) -> (Vec<u8>, Option<u8>) {
+        let offset = if full { self.data.len() * 2 } else { self.offset };
+        let result = self.data[..offset / 2].to_vec();
+        let rest = if offset % 2 == 1 { Some(self.data[offset / 2] >> 4) } else { None };
+        (result, rest)
+    }
+
     /// Create a new nibble slice with the given byte-slice with a nibble offset.
     pub fn new_offset(data: &'a [u8], offset: usize) -> Self {
         NibbleSlice { data, offset }
