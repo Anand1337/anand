8c515782e264356765701aab35b20e185627a9dd
Nikolay Kurtov <nikolay.kurtov@near.org>
2022-04-28 15:02:19 +0200

    Init txpool metric

--- chain/pool/src/lib.rs
+++ chain/pool/src/lib.rs
@@ -30,6 +30,8 @@ pub struct TransactionPool {
 impl TransactionPool {
     pub fn new(key_seed: RngSeed) -> Self {
         debug!(target: "txpool", "TransactionPool::new()");
+        // A `get()` call initializes a metric even if its value is zero.
+        metrics::TRANSACTION_POOL_TOTAL.get();
         Self {
             key_seed,
             transactions: BTreeMap::new(),
