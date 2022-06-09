4c609941b4b350edbdf22ff8895f494a355aff5e
Edvard Davtyan <davtyaneg@gmail.com>
2022-06-09 15:47:12 +0000

    .

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-START=67148923; END=67354923; cargo build -p neard --release && ./target/release/neard view_state apply_range --shard-id=1 --start-index $START --end-index $END &
+rm fn_data
+START=67148923; END=67354923; cargo build -p neard --release && ./target/release/neard view_state apply_range --shard-id=1 --start-index $START --end-index $END > fn_data &
 
 python3 e.py > output.txt
