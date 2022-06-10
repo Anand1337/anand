5a44747d65e686dbdc0238e8bb029b04506586fe
Edvard Davtyan <davtyaneg@gmail.com>
2022-06-10 14:13:57 +0000

    .

--- e.py
+++ e.py
@@ -2,7 +2,8 @@ import sys
 
 ok, fail = 0, 0
 current_cost = 16101955926
-new_cost = 140000000000
+new_cost =    140000000000
+tgas_3 =     3000000000000
 tgas_100 = 100000000000000
 new_failures, new_failures_with_extra_100tgas, new_failures_with_extra_200tgas = 0, 0, 0
 total_blocks = 0
@@ -20,7 +21,7 @@ with open(sys.argv[1]) as f:
         ok += 1
 
         real_reads = int(tokens[1])
-        remaining_gas = int(tokens[4]) - int(tokens[3])
+        remaining_gas = int(tokens[4]) - int(tokens[3]) - tgas_3
 
         extra = real_reads * (new_cost - current_cost)
         if extra > remaining_gas:
