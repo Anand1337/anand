5b65034fea0357f6daa6a90404e43797a6467dcc
Edvard Davtyan <davtyaneg@gmail.com>
2022-06-10 10:21:28 +0000

    .

--- e.py
+++ e.py
@@ -2,9 +2,12 @@ ok, fail = 0, 0
 current_cost = 16101955926
 new_cost = 140000000000
 new_failures = 0
+total_blocks = 0
 with open("fn_data") as f:
     for line in f:
         if not line.startswith("fn_fail") and not line.startswith("fn_ok"):
+            if "blocks" in line:
+                total_blocks = int(line.split(' ')[1])
             continue
 
         tokens = line.rstrip().split(' ')
@@ -21,6 +24,7 @@ with open("fn_data") as f:
             new_failures += 1
 
 
+print("total blocks:", total_blocks)
 print("total function calls:", ok + fail)
 print("current failures:", str(round(fail / (ok + fail) * 100, 2)) + "%")
 print("additional new failures:", str(round(new_failures / (ok + fail) * 100, 2)) + "%")
