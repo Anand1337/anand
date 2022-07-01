d6e2f726788d16de7bf68b6afda70dab3b528551
Edvard Davtyan <davtyaneg@gmail.com>
2022-07-01 11:16:07 +0000

    .

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ make neard
 rm fn_data_verbose
 START=56421132
 END=67421208
-./target/release/neard view_state apply_range --shard-id=0 --start-index $START --end-index $END > fn_data_verbose
+./target/release/neard view_state --readwrite apply_range --shard-id=0 --start-index $START --end-index $END > fn_data_verbose
 
 # python3 e.py > output.txt
