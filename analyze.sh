bf94a8040022696db2c3514a20269ba8b9b72166
Edvard Davtyan <davtyaneg@gmail.com>
2022-07-19 14:58:43 +0000

    .

--- analyze.sh
+++ analyze.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 rm output.txt
-for shard_id in {0,1,2,3}; do
-  echo "shard: $shard_id" >> output.txt
-  python3 e.py fn_data_verbose_$shard_id >> output.txt
-done
-
+python3 e.py fn_data_verbose >> output.txt
