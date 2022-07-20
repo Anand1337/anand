2e9c32451262adac77a100b5c586ad5923b3796d
Edvard Davtyan <davtyaneg@gmail.com>
2022-07-20 13:10:29 +0200

    .

--- core/store/src/main.rs
+++ core/store/src/main.rs
@@ -1,3 +1,5 @@
+extern crate chrono;
+
 use std::path::Path;
 use near_store::{Store, StoreConfig, DBCol, db::Mode};
 use borsh::BorshDeserialize;
@@ -9,6 +11,8 @@ use near_primitives::transaction::{ExecutionOutcomeWithIdAndProof, ExecutionMeta
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::str::FromStr;
+use std::env;
+use chrono::Local;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -25,52 +29,65 @@ fn get_height(header: &near_primitives::block_header::BlockHeader) -> u64 {
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let threads = args[1].parse::<usize>().unwrap();
+
     let lines = read_lines("./x");
     if !lines.is_ok() {
         println!(":(");
         return ();
     }
+    let lines: Vec<String> = lines.unwrap().map(|l| l.expect("Could not parse line")).collect();
 
-    let store = Store::opener(&Path::new("/home/ubuntu/.near"), &StoreConfig::default()).mode(Mode::ReadOnly).open();
-
-    /*for key_value in store.iter(DBCol::TransactionResult) {
-        if key_value.is_err() {
-            continue;
-        }
-        let (key, _) = key_value.unwrap();
-        println!("{:?}", &key);
-    }
-
-    if 2 + 2 == 4 {
-        return ();
-    }*/
+    let count = lines.len() / threads;
 
     let mut cnt = 0;
-    for line in lines.unwrap() {
-        let hash = CryptoHash::from_str(&line.unwrap()).unwrap();
-        let execution_outcomes = store.get_ser::<Vec<ExecutionOutcomeWithIdAndProof>>(DBCol::TransactionResult, &hash.0).unwrap();
-        if !execution_outcomes.is_some() {
-            println!("Can't find outcome for receipt: {:?}", &hash);
+    let mut handles = vec![];
+    for thread in 0..threads {
+        let lf = thread * count;
+        let mut rg = (thread + 1) * count;
+        if thread + 1 == threads {
+            rg = lines.len();
+        }
+        if lf == rg {
             continue;
         }
-        let execution_outcomes = execution_outcomes.unwrap();
-        assert!(execution_outcomes.len() == 1, ":(");
-        let mut heights = String::new();
-        for execution_outcome in &execution_outcomes {
-            let header = store.get_ser::<near_primitives::block_header::BlockHeader>(DBCol::BlockHeader, &execution_outcome.block_hash.0).unwrap().unwrap();
-            heights = format!("{} ", &get_height(&header));
-        }
-        println!("{} {}", &hash, heights);
-        cnt += 1;
-        if cnt % 10000 == 0 {
-            eprintln!("Progress: {}", cnt);
-        }
+        let lines_for_thread = lines[lf..rg].to_vec().clone();
+        handles.push(std::thread::spawn(move || {
+            let store = Store::opener(&Path::new("/home/edvard/.localnet-near"), &StoreConfig::default()).mode(Mode::ReadOnly).open();
+            for line in &lines_for_thread {
+                let hash = CryptoHash::from_str(&line).unwrap();
+                let execution_outcomes = store.get_ser::<Vec<ExecutionOutcomeWithIdAndProof>>(DBCol::TransactionResult, &hash.0).unwrap();
+                if !execution_outcomes.is_some() {
+                    println!("Can't find outcome for receipt: {:?}", &hash);
+                    continue;
+                }
+                let execution_outcomes = execution_outcomes.unwrap();
+                let mut heights = String::new();
+                for execution_outcome in &execution_outcomes {
+                    let header = store.get_ser::<near_primitives::block_header::BlockHeader>(DBCol::BlockHeader, &execution_outcome.block_hash.0).unwrap().unwrap();
+                    heights = format!("{}{} ", heights, &get_height(&header));
+                }
+                println!("{} {}", &hash, heights);
+                let time_now = format!("{}", Local::now());
+                cnt += 1;
+                if cnt % 10_000 == 0 {
+                    eprintln!("Time: {} progress: {} thread: {}", time_now, cnt, thread);
+                }
+            }
+        }));
+    }
+
+    for handle in handles {
+        let _ = handle.join();
     }
 
     if 2 + 2 == 4 {
         return ();
     }
 
+    let store = Store::opener(&Path::new("/home/edvard/.localnet-near"), &StoreConfig::default()).mode(Mode::ReadOnly).open();
     let mut attached_gas = HashMap::new();
     for key_value in store.iter(DBCol::Transactions) {
         if key_value.is_err() {
