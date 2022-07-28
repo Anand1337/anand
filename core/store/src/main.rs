cc40d9b03645083ba2ef72343245b2bf88927252
Edvard Davtyan <davtyaneg@gmail.com>
2022-07-28 14:56:06 +0200

    .

--- core/store/src/main.rs
+++ core/store/src/main.rs
@@ -1,14 +1,18 @@
 use std::path::Path;
-use near_store::{StoreOpener, StoreConfig, DBCol};
+use near_store::{Store, StoreConfig, DBCol};
 use borsh::BorshDeserialize;
 use near_primitives::hash::CryptoHash;
 use near_primitives::transaction::ExecutionOutcomeWithIdAndProof;
 
 fn main() {
-    let store = StoreOpener::new(&Path::new("/home/edvard/.near2"), &StoreConfig::default()).open();
+    let store = Store::opener(&Path::new("/home/edvard/.near2"), &StoreConfig::default()).open();
 
     let mut a = vec![0; 4];
-    for (key, value) in store.iter(DBCol::TransactionResult) {
+    for key_value in store.iter(DBCol::TransactionResult) {
+        if key_value.is_err() {
+            continue;
+        }
+        let (key, value) = key_value.unwrap();
         let hash = CryptoHash::try_from_slice(&key).unwrap();
         let execution_outcomes = Vec::<ExecutionOutcomeWithIdAndProof>::try_from_slice(&value).unwrap();
         a[std::cmp::min(execution_outcomes.len(), 3)] += 1;
