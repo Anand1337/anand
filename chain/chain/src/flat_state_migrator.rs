1728e3fb530936c59edab61cc57466589aa1274c
Longarithm <the.aleksandr.logunov@gmail.com>
2022-10-28 19:13:27 +0400

    add assert

--- chain/chain/src/flat_state_migrator.rs
+++ chain/chain/src/flat_state_migrator.rs
@@ -2,7 +2,7 @@ use crate::RuntimeAdapter;
 use near_primitives::types::{BlockHeight, NumShards};
 use std::sync::Arc;
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum MigrationStatus {
     SavingDeltas,
     FetchingState,
